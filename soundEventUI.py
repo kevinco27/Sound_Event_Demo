90b9063759636804f0651448d305c683d789e0d4
kevinco27 <kevinco27@hotmail.com>
2018-08-24 02:26:04 +0800

    穩定版 1. real time audio visualization 2. marking audio on wave graph by detecting sound event

--- soundEventUI.py
+++ soundEventUI.py
@@ -6,6 +6,7 @@ import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Rectangle
 import time
 
 class UI:
@@ -18,12 +19,21 @@ class UI:
         self.threadLock = threading.RLock()
         self.is_recording = False
         self.buffer_size = (args.sr//args.ws)*args.ws*args.rd*args.frame
+        self.num_windows = self.buffer_size//(args.ws)
+        self.num_frames = self.buffer_size//(args.sr*args.rd)
         self.audio_buffer = deque(np.zeros(self.buffer_size), maxlen=self.buffer_size)
-        self.colored_buffer = deque(np.zeros(self.buffer_size, dtype=np.int), maxlen=self.buffer_size)
-        # wsData_pos and wsData_in_buffer_pos_map are for tracking group of same window size data in audio buffer
-        self.wsData_pos = deque(np.zeros(self.buffer_size), maxlen=self.buffer_size//args.ws)
-        self.wsData_in_buffer_pos_map = [[idx*self.args.ws, idx*self.args.ws+(self.args.ws-1)] for idx in range(0, self.buffer_size//args.ws)]
+        self.colored_buffer = []
+        
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+        self.ax_position = self.ax.get_position()
+        self.ax.set_ylim(-0.5, 0.5)
+        self.ax.axis('off')
         self.TIME = np.linspace(0, self.buffer_size//args.sr, num=self.buffer_size)
+        self.line, = self.ax.plot(self.TIME, np.array(self.audio_buffer))
+         # wsData_pos and wsData_in_buffer_pos_map are for tracking group of same window size data in audio buffer
+        self.wsData_pos = deque(np.zeros(self.buffer_size//args.ws), maxlen=self.buffer_size//args.ws)
+        self.wsData_pos_map = np.linspace(self.ax_position.x0, self.ax_position.x1, num=self.num_windows, endpoint=False)
 
         ## User Interface widgets
         self.window_width = 1000
@@ -32,10 +42,6 @@ class UI:
         self.root.title("Sound Event")
         self.root.geometry("{}x{}".format(self.window_width, self.window_height))
         self.animate = None
-        self.fig = Figure()
-        self.ax = self.fig.add_subplot(111)
-        self.ax.set_ylim(-0.2, 0.2)
-        self.line, = self.ax.plot(self.TIME, np.array(self.audio_buffer))
         self.audio_graph = FigureCanvasTkAgg(self.fig, master=self.root)
         self.start_button = tk.Button(self.root, text="start", command=self.press_start)
         self.pause_button = tk.Button(self.root, text="pause", command=self.press_pause)
@@ -44,6 +50,7 @@ class UI:
         self.start_button.place(relwidth=0.5, relheight=0.15, relx=0.5, rely=0.85, anchor="nw")
         self.pause_button.place(relwidth=0.5, relheight=0.15, relx=0.0, rely=0.85, anchor="nw")
         self.root.mainloop()
+       
 
     def fill_audio_buffer_with_que(self):
         while self.is_recording:
@@ -51,31 +58,42 @@ class UI:
                 print("visual que size: {}".format(self.visual_que.qsize()))
                 data, timeStamp = self.visual_que.get()
                 self.audio_buffer.extendleft(data)
-                with self.threadLock: # wsData_pos and colored_buffer 
-                    self.colored_buffer.extendleft([0]*self.args.ws)
+                with self.threadLock: # wsData_pos 
                     self.wsData_pos.appendleft(timeStamp)
+                if len(self.colored_buffer)>0:
+                    for rect in self.colored_buffer:
+                        pos = rect.get_x()
+                        if pos <= self.ax_position.x1:
+                            rect.set_x(pos + self.ax_position.x1/self.num_windows)
+                        else:
+                            rect.remove()
+                            self.colored_buffer.remove(rect)
     
-    def change_audio_buffer_state_by_event(self):        
+    def mark_audio_frame_by_audio_event(self):        
         while self.is_recording:
             while not self.event_que.empty():
                 event, frame_start_time = self.event_que.get()
                 if event != "None":
                     with self.threadLock: # wsData_pos and colored_buffer 
                         try:
-                            # mark the samples in colored buffer to decide which samples to be colored
                             start_idx = self.wsData_pos.index(frame_start_time)
-                            end_idx = start_idx + self.args.sr*self.args.rd//self.args.ws
-                            start_idx_in_buffer = self.wsData_in_buffer_pos_map[start_idx][0]
-                            end_idx_in_buffer = self.wsData_in_buffer_pos_map[end_idx][1] if end_idx < len(self.wsData_pos) else self.buffer_size-1
-                            for i in range(start_idx_in_buffer, end_idx_in_buffer+1):
-                                self.colored_buffer[i] = 1
-                        except:
+                            fig_pos_x = self.wsData_pos_map[start_idx]
+                            rect = Rectangle((fig_pos_x, self.ax_position.y0+self.ax_position.height*0.25), 
+                                        self.ax_position.width/self.num_frames,
+                                        self.ax_position.height/2,
+                                        transform=self.ax.transAxes,
+                                        color= 'r',
+                                        alpha=0.3,
+                                        zorder=1000)
+                            self.ax.add_patch(rect)
+                            self.colored_buffer.append(rect)
+                        except err :
+                            print(err)
                             pass
-    
     def plot_audio_in_buffer(self, frame):
         plot_data = np.array(self.audio_buffer)
         self.line.set_data(self.TIME, plot_data)
-        return self.line
+        return [self.line] + self.colored_buffer
     
     def pause_animation(self):
         while not self.visual_que.empty():
@@ -89,7 +107,7 @@ class UI:
             self.sampler.start()
             self.detector.start()
             threading.Thread(target=self.fill_audio_buffer_with_que).start()
-            threading.Thread(target=self.change_audio_buffer_state_by_event).start()
+            threading.Thread(target=self.mark_audio_frame_by_audio_event).start()
             if self.animate is None:
                 self.animate = FuncAnimation(self.fig, 
                                             self.plot_audio_in_buffer, 
