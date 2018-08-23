89b1d7b40d84fa65cbe28a5e6fb0e463351ae3f4
kevinco27 <kevinco27@hotmail.com>
2018-08-23 16:08:29 +0800

    1. Detector with multiprocess 2. data exchanging between sampler and UI is fine

--- EventDetect.py
+++ EventDetect.py
@@ -16,22 +16,15 @@ class Detector:
         self.is_stop = Manager().Value('i', True)
 
     def _detect(self):
-        i = 0
         while(not self.is_stop.value):
-            result = random.randint(1,10)
-            result = "cry {}".format(i) if result<=5 else "None"
-            i+=1
-            self.event_que.put(result)
-            
-            # if not self.detect_que.empty():
-            #     frame = self.detect_que.get()
-            #     Time = frame[0][1] # start time stamp of the frame
-            #     # #[Testing] generate fake results
-            #     result = random.randint(1,10)
-            #     result = "cry" if result<=5 else "None"
-            #     result = [result, Time]
-            #     with  self.Lock:
-            #         self.event_que.put(result)
+            if not self.detect_que.empty():
+                frame = self.detect_que.get()
+                Time = frame[0][1] # start time stamp of the frame
+                # #[Testing] generate fake results
+                result = random.randint(1,10)
+                result = "cry" if result<=5 else "None"
+                result = [result, Time]
+                self.event_que.put(result)
     
     def start(self):
         self.is_stop.value=False
--- soundEventUI.py
+++ soundEventUI.py
@@ -28,7 +28,7 @@ class UI:
         self.animate = None
         self.fig = Figure()
         self.ax = self.fig.add_subplot(111)
-        self.ax.set_ylim(-0.5, 0.5)
+        self.ax.set_ylim(-0.2, 0.2)
         self.line, = self.ax.plot(self.TIME, np.array(self.audio_buffer))
         self.audio_graph = FigureCanvasTkAgg(self.fig, master=self.root)
         self.start_button = tk.Button(self.root, text="start", command=self.press_start)
@@ -42,16 +42,16 @@ class UI:
     def fill_audio_buffer_with_que(self):
         while self.is_recording:
             while not self.visual_que.empty():
-                # print("visual que size: {}".format(self.visual_que.qsize()))
+                print("visual que size: {}".format(self.visual_que.qsize()))
                 data, fram_start_time = self.visual_que.get()
                 self.audio_buffer.extendleft(data)
     
     def change_audio_buffer_state_by_event(self):        
         while self.is_recording:
             while not self.event_que.empty():
-                event = self.event_que.get()
+                event, frame_start_time = self.event_que.get()
                 if event != "None":
-                    print(event)
+                    pass
     
     def plot_audio_in_buffer(self, frame):
         plot_data = np.array(self.audio_buffer)
